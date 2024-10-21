0043b0200b51b47d7697b6331bea20254fb06c0d
Rahul Sutradhar <rahulsd380@gmail.com>
2024-10-21 20:46:03 +0600

    Fixed reset password issue

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -61,6 +61,7 @@ const forgetPassword = catchAsync(async (req, res) => {
 
 const resetPassword = catchAsync(async (req, res) => {
   const token = req.headers.authorization;
+  console.log(token)
   const result = await AuthServices.resetPassword(req.body, token as string);
 
   sendResponse(res, {
